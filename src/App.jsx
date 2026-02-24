3e09d19a7e54055d464dd66d39f149a0e556c236
vasanth-2003 <vasanthservey@gmail.com>
2026-02-24 17:48:32 +0530

    Add Feature A with final fixes
    Add Feature A with final fixes

--- src/App.jsx
+++ src/App.jsx
@@ -8,14 +8,7 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
+      
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
@@ -24,6 +17,7 @@ function App() {
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
+        <h1>New freature Added +1</h1>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
